b001cf8c01e7b88ca52c0716aaef259bb6ec795c
javi-domi <javdominguez@icloud.com>
2021-10-18 21:48:28 -0300

    feat: add slider

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { sliderItems } from "../data";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
-  background-color: coral;
+  // background-color: #fcad90;
   position: relative;
+  overflow: hidden;
 `;
 
 const Arrow = styled.div`
@@ -23,17 +26,85 @@ const Arrow = styled.div`
   left: ${(props) => props.direction === "left" && "10px"};
   margin: auto;
   cursor: pointer;
+  z-index: 2;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`;
+const Image = styled.img`
+  height: 80%;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 50px;
+`;
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Description = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 3px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
+  border: 2px solid black;
 `;
 
 const Slider = () => {
+  const [slideIndex, setSlideIndex] = useState(0);
+
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
   return (
     <Container>
-      <Arrow direction="right">
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <span class="material-icons">east</span>
       </Arrow>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <span class="material-icons">west</span>
       </Arrow>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => {
+          return (
+            <Slide bg={item.bg}>
+              <ImgContainer>
+                <Image src={item.img} alt="Foto" />
+              </ImgContainer>
+              <InfoContainer>
+                <Title>{item.title}</Title>
+                <Description>{item.description}</Description>
+                <Button>Shop now.</Button>
+              </InfoContainer>
+            </Slide>
+          );
+        })}
+      </Wrapper>
     </Container>
   );
 };
---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://www.ifavorpet.com/704-home_default/pet-dog-hooded-sweatshirt-clothes-colorful-brief-stripe-design.jpg",
+    title: "WINTER SALE",
+    description: "GET FLAT 30% OFF FOR NEW ARRIVALS 🧤 🧣",
+    bg: "fbffc2",
+  },
+  {
+    id: 1,
+    img: "https://www.pngkey.com/png/full/795-7958828_7-cute-and-colorful-pet-products-you-can.png",
+    title: "POPular SALE",
+    description: "GET FLAT 30% OFF FOR NEW ARRIVALS 🧤 🧣",
+    bg: "ad90fc",
+  },
+  {
+    id: 1,
+    img: "https://img-07.stickers.cloud/packs/6ec78608-4118-4718-a800-16b24a137477/webp/9106220a-da46-4b4b-aa14-26ec3b9a5619.webp",
+    title: "AUTUMN SALE",
+    description: "GET FLAT 30% OFF FOR NEW ARRIVALS 🧤 🧣",
+    bg: "fcad90",
+  },
+];
